8a6283b37f1f6f5085721af904af7e736d129b25
admin <lakshmi.patil.nine@gmail.com>
2020-01-24 11:36:25 +0530

    added: Stored all Arithmetic operations in Dictionary

--- arithmetic_computation.sh
+++ arithmetic_computation.sh
@@ -4,13 +4,15 @@ echo "Welcome to Sorting-Arithmetic-Computation"
 
 echo -e "Enter values of A B c and C : \c"
 read a b c
-operation1=$((a+b*c))
-operation2=$((a*b+c))
-operation3=$(echo "scale=2; $c+$a/$b" | bc)
-operation4=$((a%b+c))
-echo operation1 : $operation1
-echo operation2 : $operation2
-echo operation3 : $operation3
-echo operation4 : $operation4
+
+declare -A Dictionary
+Dictionary[operation1]=$((a+b*c))
+Dictionary[operation2]=$((a*b+c))
+Dictionary[operation3]=$(echo "scale=2; $c+$a/$b" | bc)
+Dictionary[operation4]=$((a%b+c))
+echo operation1 : ${Dictionary[operation1]}
+echo operation2 : ${Dictionary[operation2]}
+echo operation3 : ${Dictionary[operation3]}
+echo operation4 : ${Dictionary[operation4]}
 
 
