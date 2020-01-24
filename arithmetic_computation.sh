605fe8926574d43a51e62442565154ff6ce22321
admin <lakshmi.patil.nine@gmail.com>
2020-01-24 11:12:49 +0530

    added: calculated 2nd Arithmetic operation

--- arithmetic_computation.sh
+++ arithmetic_computation.sh
@@ -6,7 +6,6 @@ echo -e "Enter values of A B and C : \c"
 read a b c
 operation1=$((a+b*c))
 operation2=$((a*b+c))
-operation3=$(echo "scale=2;$c+$a/$b" | bc)
 echo operation1 : $operation1
 echo operation2 : $operation2
-echo operation3 : $operation3
+
