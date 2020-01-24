d22cfc6458828ce1351f5513b54678175dcb462d
admin <lakshmi.patil.nine@gmail.com>
2020-01-24 11:53:01 +0530

    added: reading Dictionary element into array

--- arithmetic_computation.sh
+++ arithmetic_computation.sh
@@ -1,18 +1,33 @@
 #!/bin/bash -x
 
+# welcome message
 echo "Welcome to Sorting-Arithmetic-Computation"
 
+# reading three inputs
 echo -e "Enter values of A B c and C : \c"
 read a b c
 
+# declaring dictionary
 declare -A Dictionary
+
+# storing all operations into dictionary
 Dictionary[operation1]=$((a+b*c))
 Dictionary[operation2]=$((a*b+c))
 Dictionary[operation3]=$(echo "scale=2; $c+$a/$b" | bc)
 Dictionary[operation4]=$((a%b+c))
-echo operation1 : ${Dictionary[operation1]}
-echo operation2 : ${Dictionary[operation2]}
-echo operation3 : ${Dictionary[operation3]}
-echo operation4 : ${Dictionary[operation4]}
+
+# reading dictionary into array
+DictionaryToArray()
+{
+	for((i=0; i<4; i++))
+	do
+		arr[i]=${Dictionary[operation$((i+1))]}
+	done
+}
+
+DictionaryToArray
+# printing all operations in dictionary 
+echo  ${arr[@]}
+
 
 
