e02b98c99373e6804d95f74ae91d0ac1ad9e1596
admin <lakshmi.patil.nine@gmail.com>
2020-01-24 10:47:24 +0530

    added: calculated 1st Arithmetic operation

--- arithmetic_computation.sh
+++ arithmetic_computation.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "Welcome to Sorting-Arithmetic-Computation"
 
 echo -e "Enter values of A B and C : \c"
 read a b c
-echo "Values are =  A:$a B:$b C:$c "
+result=$((a+b*c))
+echo Result is: $result
