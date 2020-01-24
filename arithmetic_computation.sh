b363decfffec3a6ee9f24a06ba41ea5fcb8d5c2e
admin <lakshmi.patil.nine@gmail.com>
2020-01-24 11:23:43 +0530

    added: calculated 4th Arithmetic operation

--- arithmetic_computation.sh
+++ arithmetic_computation.sh
@@ -2,10 +2,15 @@
 
 echo "Welcome to Sorting-Arithmetic-Computation"
 
-echo -e "Enter values of A B and C : \c"
+echo -e "Enter values of A B c and C : \c"
 read a b c
 operation1=$((a+b*c))
 operation2=$((a*b+c))
+operation3=$(echo "scale=2; $c+$a/$b" | bc)
+operation4=$((a%b+c))
 echo operation1 : $operation1
 echo operation2 : $operation2
+echo operation3 : $operation3
+echo operation4 : $operation4
+
 
