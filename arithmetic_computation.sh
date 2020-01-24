af4454e4094f7b5e07cda15891637066536587ee
admin <lakshmi.patil.nine@gmail.com>
2020-01-24 11:09:43 +0530

    added: calculated 3rd Arithmetic operation

--- arithmetic_computation.sh
+++ arithmetic_computation.sh
@@ -4,5 +4,9 @@ echo "Welcome to Sorting-Arithmetic-Computation"
 
 echo -e "Enter values of A B and C : \c"
 read a b c
-result=$((a*b+c))
-echo Result is: $result
+operation1=$((a+b*c))
+operation2=$((a*b+c))
+operation3=$(echo "scale=2;$c+$a/$b" | bc)
+echo operation1 : $operation1
+echo operation2 : $operation2
+echo operation3 : $operation3
